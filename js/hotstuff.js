2019dc88caaa256de8421f2ea81cb1b712782dbd
John Anderson <sontek@gmail.com>
2014-03-01 00:55:38 -0600

    sd

--- js/hotstuff.js
+++ js/hotstuff.js
@@ -1,3 +1,25 @@
+if (( typeof cordova == 'undefined') && ( typeof Cordova == 'undefined'))
+		alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+	if ( typeof CDV == 'undefined')
+		alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+	if ( typeof FB == 'undefined')
+		alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+	FB.Event.subscribe('auth.login', function(response) {
+		alert('auth.login event');
+	});
+
+	FB.Event.subscribe('auth.logout', function(response) {
+		alert('auth.logout event');
+	});
+
+	FB.Event.subscribe('auth.sessionChange', function(response) {
+		alert('auth.sessionChange event');
+	});
+
+	FB.Event.subscribe('auth.statusChange', function(response) {
+		alert('auth.statusChange event');
+	});
 
 document.addEventListener("deviceready", onDeviceReady, false);
 	document.addEventListener('deviceready', function() {
@@ -26,29 +48,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		alert("resume");
 	}
 	
-	if (( typeof cordova == 'undefined') && ( typeof Cordova == 'undefined'))
-		alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
-	if ( typeof CDV == 'undefined')
-		alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
-	if ( typeof FB == 'undefined')
-		alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
-
-	FB.Event.subscribe('auth.login', function(response) {
-		alert('auth.login event');
-	});
-
-	FB.Event.subscribe('auth.logout', function(response) {
-		alert('auth.logout event');
-	});
-
-	FB.Event.subscribe('auth.sessionChange', function(response) {
-		alert('auth.sessionChange event');
-	});
-
-	FB.Event.subscribe('auth.statusChange', function(response) {
-		alert('auth.statusChange event');
-	});
-
 	/*function getSession() {
 	 alert("session: " + JSON.stringify(FB.getSession()));
 	 }
