64cd92180e77f1a7757c2ed944318160e21cf849
John Anderson <sontek@gmail.com>
2014-03-01 00:50:37 -0600

    sd

---
+++ js/hotstuff.js
@@ -0,0 +1,83 @@
+
+document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener('deviceready', function() {
+		try {
+			document.addEventListener("pause", onPause, false);
+			document.addEventListener("resume", onResume, false);
+			alert('Device is ready! Make sure you set your app_id below this alert.');
+			FB.init({
+				appId : "346598232144666",
+				nativeInterface : CDV.FB,
+				useCachedDialogs : false
+			});
+			document.getElementById('data').innerHTML = "";
+		} catch (e) {
+			alert(e);
+		}
+	}, false); 
+	
+	//What to do when paused
+	function onPause() {
+		alert("paused!");
+	}
+
+	//What to do when resumed
+	function onResume() {
+		alert("resume");
+	}
+	
+	if (( typeof cordova == 'undefined') && ( typeof Cordova == 'undefined'))
+		alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+	if ( typeof CDV == 'undefined')
+		alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+	if ( typeof FB == 'undefined')
+		alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+	FB.Event.subscribe('auth.login', function(response) {
+		alert('auth.login event');
+	});
+
+	FB.Event.subscribe('auth.logout', function(response) {
+		alert('auth.logout event');
+	});
+
+	FB.Event.subscribe('auth.sessionChange', function(response) {
+		alert('auth.sessionChange event');
+	});
+
+	FB.Event.subscribe('auth.statusChange', function(response) {
+		alert('auth.statusChange event');
+	});
+
+	/*function getSession() {
+	 alert("session: " + JSON.stringify(FB.getSession()));
+	 }
+	 */
+	function getLoginStatus() {
+		FB.getLoginStatus(function(response) {
+			if (response.status == 'connected') {
+				alert('logged in');
+			} else {
+				alert('not logged in');
+			}
+		});
+	}
+
+	
+	function logout() {
+		FB.logout(function(response) {
+			alert('logged out');
+		});
+	}
+
+	function login() {
+		FB.login(function(response) {
+			if (response.session) {
+				alert('logged in');
+			} else {
+				alert('not logged in');
+			}
+		}, {
+			scope : "email"
+		});
+	}
